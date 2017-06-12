f76b2fe6b29cf31a6a63f0ff1e21584bc0d29c65
meldelapaz <meldelapaz.17@gmail.com>
2017-06-12 14:46:54 -0400

    tried to push the list to storage

--- assets/javascript/newlist.js
+++ assets/javascript/newlist.js
@@ -1,86 +1,3 @@
-// Pseudo Code
-
-// Basic HTML
-// Document.ready
-
-// on click of search button on user input form
-    // Get user input
-    // Display searched items in "Aopend: Products"
-    // Enable scroll arrows
-
-
-// on click of appended product search item
-    // Append to middle section/row
-
-// Questions to calculate form
-    // user input on how long until they want it back on their list
-    // Maybe have an "ADD NOW" button(?) and a "1 TIME ONLY ADD" button (?)
-
-
-// on click function when user adds products
-    // Wait for the selected amount of time to pass
-    // Append product to Grocery List row
-    // + button to add grocery item to list
-
-// If the users search gives no results
-    // Append word to list
-
-//----------------------------------------------------------------------------------------------------------------------
-
-// List of variables and classes/Ids so we all use the same ones
-
-// VARIABLES:
-    // var user-input (the users search)
-    // var queryURL (the API url)
-    // var productOptions (the first row of search results)
-    // var p (p tag to store the item names)
-    // var productSelected (second row where the users chosen product shows up)
-    // var groceryList (when time is up, the product appends to grocery list div)
-    // var timeOptions (the options of time the user has)
-    // var time (time user has chosen until it appends to the list)
-    // var timesUp (when the time is up)
-    // var coupons (the coupons we find)
-
-// CLASSES:
-    // class = searchForm
-    // class = productOptions (img class)
-    // class = productSelected (img class)
-    // class = addTime (the amount of time the user selected)
-    // class = newProduct (when a new product is added, we prepend it to the list)
-
-
-//----------------------------------------------------------------------------------------------------------------------
-
-// THINGS TO REMEMBER:
-
-// TEST EVERYTHING
-// CONSOLE LOG EVERYTHING
-
-//----------------------------------------------------------------------------------------------------------------------
-
-// WALMART API DOCS
-
-/* PARAMETERS
-apiKey	Your API access key.
-format	Type of response required, allowed values [json, xml]. Default is json.
-ids	Comma separated list of item ids
-
-API KEY: 3xy3dz4kywkwwkkxtjsqv9fj*/
-
-
-// WIKIPEDIA API DOCS
-
-// PARAMETERS
-
-// GOOGLE MAPS API
-
-// PARAMETERS AND RATE LIMIT
-
-/* RATE LIMIT: 25,000 map loads per 24 hours */
-
-
-//----------------------------------------------------------------------------------------------------------------------
-
 
 // Document ready function
 $(document).ready (function() {
@@ -135,12 +52,30 @@ $(document).ready (function() {
         console.log(queryURLWiki);
 
 
+        var responses = [];
+        var query = "peanut butter";
+
         // Pulling AJAX request from Walmart API
         $.ajax({
             url: queryURLWal
         }).done(function (response) {
-            console.log(response);
+            console.log("THISREPONSEHERE",response);
 
+            for (let i = 0; i < response.items.length; i++) {
+                
+
+                var temp = response.items[i]
+                var items = {
+                    name: response.items.name,
+                    mediumImage: response.items.mediumImage
+            
+
+            
+            responses.push(items);
+            responses = JSON.stringify(responses);
+            localStorage.setItem(query,responses);
+    };
+            }
             // Transfer content to HTML
             $(".productOptions1").html("<div class='divproduct'> <img src=" + response.items["0"].mediumImage + " class = proImage data-selected = " + response.items["0"].mediumImage + ">" + "<br>" + response.items["0"].name + "</div>");
             $(".productOptions2").html("<div class='divproduct'> <img src=" + response.items["1"].mediumImage + " class = proImage data-selected = " + response.items["1"].mediumImage + ">" + "<br>" + response.items["1"].name + "</div>");
@@ -151,6 +86,7 @@ $(document).ready (function() {
         $('#productsOptionTitle').removeClass('hideOptions');
 
         // Pulling AJAX request from Wikipedia API
+        
         $.ajax({
             url: queryURLWiki
         }).done(function (response) {
@@ -193,29 +129,29 @@ $(document).ready (function() {
             var timerId = setInterval(countdown, 1000);
 
             // if statement to determine if the browser has local storage
-            if (window.localStorage) {
+            // if (window.localStorage) {
 
-                // show the product selected when the time runs out
-                productSelected++;
+            //     // show the product selected when the time runs out
+            //     productSelected++;
 
-                // set the item
-                window.localStorage.setItem('productSelected', productSelected);
+            //     // set the item
+            //     window.localStorage.setItem('productSelected', productSelected);
 
-                // push it to html
-                $productSelected.html(productSelected);
+            //     // push it to html
+            //     $productSelected.html(productSelected);
 
-                /*productSelected = window.localStorage.getItem('productSelected');
+            //     /*productSelected = window.localStorage.getItem('productSelected');
 
-                 if (typeof productSelected === 'object') {
+            //      if (typeof productSelected === 'object') {
 
-                 productSelected = 0;
+            //      productSelected = 0;
 
-                 }*/
-            }
-                if (window.localStorage) {
-                    window.localStorage.setItem('savelist', savelist);
-                    $savelist.html(savelist)
-                    }
+            //      }*/
+            // }
+                // if (window.localStorage) {
+                //     window.localStorage.setItem('savelist', savelist);
+                //     $savelist.html(savelist)
+                //     }
 
 
             function countdown() {
@@ -290,6 +226,3 @@ $(document).ready (function() {
 
 
 });
-
-
-    
